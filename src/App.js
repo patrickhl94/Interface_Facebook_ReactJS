55593a7d462a7d0f09a5e4adb971b124b131f7e2
Patrick Lima <patrickhl94@hotmail.com>
2020-03-24 22:06:58 -0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './style/body.css';
 import Header from './components/Header'
+import PostList from './components/PostList';
 
 function App() {
 
   return (
     <>
       <Header />
+      <PostList />
     </>
   )
 
