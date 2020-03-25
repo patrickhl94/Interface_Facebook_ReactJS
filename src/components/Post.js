046a92afb20660ee044f89a0fb436b8e8c7bde3c
Patrick Lima <patrickhl94@hotmail.com>
2020-03-24 22:07:09 -0300

    Created Post.js

---
+++ src/components/Post.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import '../style/post.css'
+
+class Post extends Component {
+
+  render() {
+
+    return (
+      <div class="container-post">
+       
+        <div class="area-profile">
+          <img src="https://gpluseurope.com/wp-content/uploads/Mauro-profile-picture.jpg" alt="" srcset=""/>
+          <div>
+            <div class='name-profile'>Patrick Lima G</div>
+            <small class='data-profile'>25 Mar 2020</small>
+          </div>
+        </div>
+   
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+   
+      </div>
+    )
+  }
+
+}
+
+export default Post;
